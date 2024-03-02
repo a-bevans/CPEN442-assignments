b599ed250b8748c45d2aae09c310a6e7ba271997
a-bevans <a.b.evans@outlook.com>
2024-03-01 17:15:13 -0800

    assigned tasks

--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -1,24 +1,29 @@
 class Protocol:
     # Initializer (Called from app.py)
     # TODO: MODIFY ARGUMENTS AND LOGIC AS YOU SEEM FIT
+    # Generate g, p, and R_a/R_b
+    # Nadia
     def __init__(self):
         self._key = None
         pass
 
 
     # Creating the initial message of your protocol (to be send to the other party to bootstrap the protocol)
+    # Nadia
     # TODO: IMPLEMENT THE LOGIC (MODIFY THE INPUT ARGUMENTS AS YOU SEEM FIT)
     def GetProtocolInitiationMessage(self):
         return ""
 
 
     # Checking if a received message is part of your protocol (called from app.py)
+    # Alex
     # TODO: IMPLMENET THE LOGIC
     def IsMessagePartOfProtocol(self, message):
         return False
 
 
     # Processing protocol message
+    # Alex
     # TODO: IMPLMENET THE LOGIC (CALL SetSessionKey ONCE YOU HAVE THE KEY ESTABLISHED)
     # THROW EXCEPTION IF AUTHENTICATION FAILS
     def ProcessReceivedProtocolMessage(self, message):
@@ -26,6 +31,7 @@ class Protocol:
 
 
     # Setting the key for the current session
+    # Alex
     # TODO: MODIFY AS YOU SEEM FIT
     def SetSessionKey(self, key):
         self._key = key
@@ -33,6 +39,7 @@ class Protocol:
 
 
     # Encrypting messages
+    # Claris
     # TODO: IMPLEMENT ENCRYPTION WITH THE SESSION KEY (ALSO INCLUDE ANY NECESSARY INFO IN THE ENCRYPTED MESSAGE FOR INTEGRITY PROTECTION)
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
     def EncryptAndProtectMessage(self, plain_text):
@@ -41,6 +48,7 @@ class Protocol:
 
 
     # Decrypting and verifying messages
+    # Claris
     # TODO: IMPLEMENT DECRYPTION AND INTEGRITY CHECK WITH THE SESSION KEY
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
     def DecryptAndVerifyMessage(self, cipher_text):
