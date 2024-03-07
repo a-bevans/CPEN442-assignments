85be83772b9a5d7b278681a06fa31e402dd6286a
noodnoof <nadianafeesa@gmail.com>
2024-03-07 00:49:24 -0800

    worked on constructor of Protocol class and GetProtocolInitiationMessage

--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -1,18 +1,39 @@
+import random
+
+
 class Protocol:
+    
     # Initializer (Called from app.py)
     # TODO: MODIFY ARGUMENTS AND LOGIC AS YOU SEEM FIT
     # Generate g, p, and R_a/R_b
     # Nadia
     def __init__(self):
-        self._key = None
-        pass
+        """
+        Initializes the Protocol class with default values for g, p, and R.
 
+        Attributes:
+        - g (int): The generator value.
+        - p (int): The prime modulus.
+        - R (int): A nonce value used in the protocol.
+        """
+        self._key = None
+        self.g = 5  # i am unsure about this value
+        self.p = 23  # i am unsure about this value
+        self.R = None
 
     # Creating the initial message of your protocol (to be send to the other party to bootstrap the protocol)
     # Nadia
     # TODO: IMPLEMENT THE LOGIC (MODIFY THE INPUT ARGUMENTS AS YOU SEEM FIT)
     def GetProtocolInitiationMessage(self):
-        return ""
+        """
+        Generates the protocol initiation message containing g, p, and R.
+
+        Returns:
+        - str: A string containing g, p, and R separated by commas.
+        """
+        # Generate a random value R within the range (1, p-1) as the nonce
+        self.R = random.randint(1, self.p - 1)
+        return f"{self.g},{self.p},{self.R}"
 
 
     # Checking if a received message is part of your protocol (called from app.py)
